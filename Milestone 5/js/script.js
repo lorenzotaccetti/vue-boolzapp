d1b0ae4a9d10890f4311cb935bf7dd584461219b
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-03-05 13:18:42 +0100

    Boolzapp Repo

--- Milestone 5/js/script.js
+++ Milestone 5/js/script.js
@@ -1,6 +1,4 @@
 // Milestone 5
-// Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
-// Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti.
 
 Vue.config.devtools = true;
 const app = new Vue({
